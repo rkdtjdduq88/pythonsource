17315f9c3f283665b06d318e9c6e0b279eb3c314
rkdtjdduq88 <rkdtjdduq88@naver.com>
2023-07-05 18:10:26 +0900

    파이썬 수업자료

--- ch2/dict1.py
+++ ch2/dict1.py
@@ -49,3 +49,63 @@ print(dict1)
 # get(키값)
 print(dict1.get("name"))
 print(dict1.get("addr")) # None
+
+# keys() : 딕셔너리 내 키 값 가져오기
+print(dict1.keys()) # dict_keys(['name', 'age'])
+
+# values()
+print(dict1.values()) # dict_values(['park', 12])
+
+# items() : key, value 쌍으로 가져오기
+print(dict1.items()) # dict_items([('name', 'park'), ('age', 12)])
+
+# in : 키가 포함되어 있는가?
+print("name" in dict1) # True
+print(4 in dict2) # False
+
+my_info = {"name":"kim","age":35,"city":"seoul"}
+
+for k in my_info.keys():
+    print(k)
+for v in my_info.values():
+    print(v)
+for k,v in my_info.items():
+    print(k,v)
+
+info = {v for v in my_info.values()} # {'seoul', 35, 'kim'}
+print(info)
+
+info = [v for v in my_info.values()] # ['kim', 35, 'seoul']
+print(info)
+
+info = {(k,v) for k,v in my_info.items()} # {('age', 35), ('name', 'kim'), ('city', 'seoul')}
+print(info)
+
+
+# 딕셔너리 생성하기
+# "A" = 90, "B" = 80, "C" = 70 등급을 딕셔너리로 생성하기
+dict1 = {"A":90, "B":80, "C":70}
+# B 키에 해당하는 값 출력하기
+print(dict1["B"])
+print(dict1.get("B"))
+# B 키 값을 삭제한 후 딕셔너리 출력하기
+del dict1["B"]
+print(dict1)
+# 성인 : 10000, 청소년 : 7000, 아동 : 3000 데이터를 딕셔너리 생성
+dict2 = {"성인":10000, "청소년":7000,"아동":3000}
+# 위 딕셔너리에 소아 : 0 항목 추가
+dict2["소아"] = 0
+# 키 값만 출력하기
+print(dict2.keys()) # dict_keys(['성인', '청소년', '아동', '소아'])
+# value 값만 출력하기
+print(dict2.values()) # dict_values([10000, 7000, 3000, 0])
+
+# list(), tuple() : 딕셔너리에서 리스트 or 튜플로 변환하기
+print(list(dict2.keys())) # ['성인', '청소년', '아동', '소아']
+print(tuple(dict2.keys())) # ('성인', '청소년', '아동', '소아')
+
+print(type(dict1))
+
+# key, value 전부 지우기
+dict1.clear()
+print(dict1)
